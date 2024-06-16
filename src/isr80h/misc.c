f0a114b88dfbdc68dd56aea087fb6e1468965354
Neeraj Gupta <neerajg1320@gmail.com>
2024-06-16 12:25:08 +0530

    Our kernel command invocation code is workinggit add .

--- src/isr80h/misc.c
+++ src/isr80h/misc.c
@@ -1,7 +1,10 @@
 #include "misc.h"
 #include "idt/idt.h"
+#include "terminal/print.h"
 
 void* isr80h_command0_sum(struct interrupt_frame* frame)
 {
+    print("isr80h_command0_sum: invoked\n");
+
     return 0;
 }
